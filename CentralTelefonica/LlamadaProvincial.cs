36f9f5b0a77bfb265a1394eaf5a8fe91eab765b9
DanCesRom <123031318+DanCesRom@users.noreply.github.com>
2023-03-14 04:15:29 -0400

    Add files via upload

---
+++ CentralTelefonica/LlamadaProvincial.cs
@@ -0,0 +1,22 @@
+﻿namespace CentralTelefonica
+{
+    public class LlamadaProvincial
+    {
+        public virtual double Precio1()
+        {
+            return 20;
+        }
+        public virtual double Precio2()
+        {
+            return 25;
+        }
+        public virtual double Precio3()
+        {
+            return 30;
+        }
+
+        public  int Franja {get; set;}
+
+
+    }
+}
